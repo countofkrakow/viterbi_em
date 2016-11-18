5aa03a7a86dbe5ac88ba43ddb586b32abce282d4
countofkrakow <chrisnakovski@gmail.com>
2016-11-17 23:47:45 -0800

    It works!

--- src/viterbi.py
+++ src/viterbi.py
@@ -2,28 +2,57 @@ from data import parse_fna
 from math import log, exp
 import sys
 import json
+from collections import deque
 
 def run_viterbi(gene, params):
 
 
     a = log(params['transitions']['begin'][0]) + log(params['emissions']['state_1'][raw_gene[0]])
     b = log(params['transitions']['begin'][1]) + log(params['emissions']['state_2'][raw_gene[0]])
-    hmm_model = [(a, b)]
+    hmm_model = [((a, False), (b, False))]
 
     for i in range(1, len(raw_gene)):
-        prev_a, prev_b = hmm_model[i-1]
-
+        at, bt = hmm_model[i-1]
+        prev_a, prob_a = at
+        prev_b, prob_b = bt
         a_stay = prev_a + log(params['transitions']['state_1'][0]) + log(params['emissions']['state_1'][raw_gene[i]])
         a_trans = prev_b + log(params['transitions']['state_2'][0]) + log(params['emissions']['state_1'][raw_gene[i]])
-        a = max(a_stay, a_trans)
+        a = (max(a_stay, a_trans), a_trans <= a_stay)
 
         b_stay = prev_b + log(params['transitions']['state_2'][1]) + log(params['emissions']['state_2'][raw_gene[i]])
         b_trans = prev_a + log(params['transitions']['state_1'][1]) + log(params['emissions']['state_2'][raw_gene[i]])
-        b = max(b_stay, b_trans)
+        b = (max(b_stay, b_trans), b_stay <= b_trans)
 
+        # true if coming from state 1. false otherwise
         hmm_model.append((a, b))
 
     return hmm_model
+
+def traceback(hmm):
+    ret = deque()
+    i = len(hmm) - 2
+    a, b = hmm[-1]
+    prob_a, before_a = a
+    prob_b, before_b = b
+
+    if prob_a >= prob_b:
+        prev_state_not_cpg = before_a
+        ret.append(True)
+    else:
+        prev_state_not_cpg = before_b
+        ret.append(False)
+
+    while i > 0:
+        if prev_state_not_cpg:
+            a, b = hmm[i]
+            prob_a, prev_state_not_cpg = a
+            ret.appendleft(True)
+        else:
+            a, b = hmm[i]
+            prob_b, prev_state_not_cpg = b
+            ret.appendleft(False)
+        i -= 1
+    return [e for e in ret]
 
 def run_em(hmm, gene):
     s1_stay_count = 0
@@ -90,15 +119,15 @@ def run_em(hmm, gene):
 
 # state 1 represents normal nucleotide distributions
 # state 2 represents cpg islands
-def find_cpg_islands(hmm, k):
+def find_cpg_islands(states, k):
     in_cpg = False
     ret = []
     i = 0
     start = None
-    while k != 0 and i < len(hmm):
-        a, b = hmm[i]
+    while k != 0 and i < len(states):
         # not in a cpg island
-        if a >= b:
+        not_cpg = states[i]
+        if not_cpg:
             # exiting cpg island
             if in_cpg:
                 in_cpg = False
@@ -127,7 +156,7 @@ if __name__ == '__main__':
 
     for i in range(9):
         hmm = run_viterbi(raw_gene, params)
-
+        states = traceback(hmm)
         print('Transition parameters:')
         print('State 1: %f %f' % (params['transitions']['state_1'][0], params['transitions']['state_1'][1]))
         print('State 2: %f %f' % (params['transitions']['state_2'][0], params['transitions']['state_2'][1]))
@@ -144,8 +173,8 @@ if __name__ == '__main__':
         C = params['emissions']['state_2']['C']
         T = params['emissions']['state_2']['T']
         print('State_2:   %f\t%f\t%f\t%f' % (A, G, C, T))
-        print('Log probability of most likely path: %f' % max(hmm[-1][1], hmm[-1][0]))
-        cpg_islands = find_cpg_islands(hmm, 5)
+        print('Log probability of most likely path: %f' % max(hmm[-1][1][0], hmm[-1][0][0]))
+        cpg_islands = find_cpg_islands(states, 5)
         print('Number of hits: %d' % len(cpg_islands))
         for start, end in cpg_islands:
             print('\t%d, %d' % (start, end))
